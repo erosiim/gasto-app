fc27ff4704ca6ef6edc4fae1cf0e3e85117d7edf
Alonso Escamilla <exkapp@gmail.com>
2024-06-06 19:27:54 -0600

    Setup for Android app

---
+++ android/app/src/main/java/com/eros/gasto/MainActivity.java
@@ -0,0 +1,5 @@
+package com.eros.gasto;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.eros.gasto',
+  appName: 'gasto app',
+  webDir: 'C:/Users/Surface/Documents/erosiim/e-gasto-app/dist/e-gasto-app/browser/'
+};
+
+export default config;
