8a7bebe5bc2961053cb57283ceeb6ade0b9518e0
Alonso Escamilla <exkapp@gmail.com>
2024-06-12 20:25:44 -0600

    Added formGroup to ItemForm

--- src/app/shared/ui/new-item-form/new-item-form.component.ts
+++ src/app/shared/ui/new-item-form/new-item-form.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import {
-  MatDialog,
-  MatDialogRef,
   MatDialogActions,
   MatDialogClose,
-  MatDialogTitle,
   MatDialogContent,
+  MatDialogTitle
 } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import {MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 @Component({
   selector: 'app-new-item-form',
   standalone: true,
@@ -24,9 +23,35 @@ import {MatSelectModule} from '@angular/material/select';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatSelectModule
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   templateUrl: './new-item-form.component.html',
   styleUrl: './new-item-form.component.css',
 })
-export class NewItemFormComponent {}
+export class NewItemFormComponent {
+  
+  itemForm = new FormGroup({
+    itemName : new FormControl(''),
+    typeOfItem : new FormControl(''),
+    quantityOfItem : new FormControl(0),
+    itemPrice : new FormControl(0.0),
+    totalPriceForItem : new FormControl(0)
+  })
+  constructor (){}
+  
+  showItem(){
+    console.log(this.itemForm.value)
+  }
+
+  getTotalForItem(){
+    const totalPriceForItem = this.itemForm.value.itemPrice!  * this.itemForm.value.quantityOfItem! 
+    this.itemForm.patchValue({
+      totalPriceForItem: totalPriceForItem
+    })
+    console.log(totalPriceForItem)
+  }
+
+
+
+}
